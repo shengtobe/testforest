bc02f0421e66e1696ea59f824b8c3d90b2f50bca
it0104 <IT0104@icsc.com.tw>
2020-10-23 08:41:22 +0800

    監控系統 router

--- src/router/moduleRouter/monitor.js
+++ src/router/moduleRouter/monitor.js
@@ -2,8 +2,13 @@
 
 export const MonitorRouter = [
     {
-        linkText: '監控系統',
-        path: '/monitor',
-        component: () => import('@/views/Monitor.vue')
+        linkText: '顛坡監控',
+        path: '/monitor/slope-monitor',
+        component: () => import('@/views/monitor/slopeMonitor.vue')
+    },
+    {
+        linkText: '雨量監測',
+        path: '/monitor/rainfall-monitor',
+        component: () => import('@/views/monitor/rainfallMonitor.vue')
     },
 ]
