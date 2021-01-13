d584bf7bd07c66078401ffea5f7e93610d8da7ba
Alvin <lcndance1999@gmail.com>
2021-01-13 17:12:23 +0800

    修改行車安全資訊 TopBasicTable 組件的用法

--- src/views/smis/carSafeInfo/info/ReviewComplated.vue
+++ src/views/smis/carSafeInfo/info/ReviewComplated.vue
@@ -1,41 +1,13 @@
 <template>
 <v-container style="max-width: 1200px">
-    <h2 class="mb-4">安全資訊編號：{{ routeId }}</h2>
+    <h2 class="mb-4">安全資訊編號：{{ id }}</h2>
 
     <!-- 上面的欄位 -->
     <TopBasicTable :items="topItems" />
 
     <!-- 下面的欄位 -->
     <v-row no-gutters class="mt-8">
-        <v-col cols="12" style="border-bottom: 1px solid #CFD8DC">
-            <v-row no-gutters>
-                <v-col class="yellow lighten-3 pl-3 pb-2 pt-3"
-                    style="max-width: 160px"
-                >
-                    <span class="font-weight-black">
-                        <v-icon class="mr-1 mb-1">mdi-file-document</v-icon>通報主題
-                    </span>
-                </v-col>
-
-                <v-col class="white pa-3">{{ title }}</v-col>
-            </v-row>
-        </v-col>
-
-        <v-col cols="12" style="border-bottom: 1px solid #CFD8DC">
-            <v-row no-gutters>
-                <v-col class="yellow lighten-3 pl-3 pb-2 pt-3"
-                    style="max-width: 160px"
-                >
-                    <span class="font-weight-black">
-                        <v-icon class="mr-1 mb-1">mdi-file-document</v-icon>發布內容
-                    </span>
-                </v-col>
-
-                <v-col class="white pa-3"
-                    v-html="desc"
-                ></v-col>
-            </v-row>
-        </v-col>
+        <BottomTable :items="bottomItems" />
 
         <v-col cols="12" style="border-bottom: 1px solid #CFD8DC">
             <v-row no-gutters>
@@ -57,48 +29,6 @@
                         {{ item.fileName }}
                     </v-chip>
                 </v-col>
-            </v-row>
-        </v-col>
-
-        <v-col cols="12" style="border-bottom: 1px solid #CFD8DC">
-            <v-row no-gutters>
-                <v-col class="yellow lighten-3 pl-3 pb-2 pt-3"
-                    style="max-width: 160px"
-                >
-                    <span class="font-weight-black">
-                        <v-icon class="mr-1 mb-1">mdi-account-multiple</v-icon>收件人
-                    </span>
-                </v-col>
-
-                <v-col class="white pa-3">{{ recipients }}</v-col>
-            </v-row>
-        </v-col>
-
-        <v-col cols="12" style="border-bottom: 1px solid #CFD8DC">
-            <v-row no-gutters>
-                <v-col class="yellow lighten-3 pl-3 pb-2 pt-3"
-                    style="max-width: 160px"
-                >
-                    <span class="font-weight-black">
-                        <v-icon class="mr-1 mb-1">mdi-account-multiple</v-icon>副本
-                    </span>
-                </v-col>
-
-                <v-col class="white pa-3">{{ cc }}</v-col>
-            </v-row>
-        </v-col>
-
-        <v-col cols="12" style="border-bottom: 1px solid #CFD8DC">
-            <v-row no-gutters>
-                <v-col class="yellow lighten-3 pl-3 pb-2 pt-3"
-                    style="max-width: 160px"
-                >
-                    <span class="font-weight-black">
-                        <v-icon class="mr-1 mb-1">mdi-account-multiple</v-icon>加會人
-                    </span>
-                </v-col>
-
-                <v-col class="white pa-3">{{ joiners }}</v-col>
             </v-row>
         </v-col>
 
@@ -242,15 +172,6 @@
                             <div v-if="item.isRead == '已讀'">{{ item.isRead }}</div>
                             <div v-else class="error--text">{{ item.isRead }}</div>
                         </template>
-
-                        <!-- 頁碼 -->
-                        <template v-slot:footer="footer">
-                            <Pagination
-                                :footer="footer"
-                                :pageOpt="pageOpt"
-                                @chPage="chPage"
-                            />
-                        </template>
                     </v-data-table>
                 </v-card>
             </v-col>
@@ -303,39 +224,22 @@
 <script>
 import { mapActions } from 'vuex'
 import TopBasicTable from '@/components/TopBasicTable.vue'
+import BottomTable from '@/components/BottomTable.vue'
 
 export default {
-    props: ['closeStatus'],  // 測試用屬性
+    props: ['itemData'],
     data: () => ({
-        routeId: '',
+        id: '',  // 編號
         done: false,  // 是否完成頁面操作
         status: '',  // 處理狀態
-        topItems: {  // 上面的欄位
-            depart: { icon: 'mdi-bank', title: '通報單位', text: '' },
-            name: { icon: 'mdi-account', title: '通報人', text: '' },
-            status: { icon: 'mdi-ray-vertex', title: '發布狀態', text: '' },
-        },
-        title: '',  // 通報主題
-        desc: '',  // 發布內容
-        recipients: '',  // 收件人
-        cc: '',  // 副本
-        joiners: '',  // 加會人
+        topItems: [],  // 上面的欄位
+        bottomItems: [],  // 下面的欄位
         files: [],  // 檔案附件
         opinion: '',  // 加會意見
         opinionList: [],  // 意見列表
         isLoading: false,  // 退回 dialog 是否讀取中
         dialog: false,  // 退回 dialog 是否顯示
         backReason: '',  // 退回原因
-        members: [  // 所有員工資料
-            { name: '趙國強 (K59632)', value: '1'},
-            { name: '錢光華 (K12584)', value: '2'},
-            { name: '孫文豪 (K69584)', value: '3'},
-            { name: '李運深 (K24785)', value: '4'},
-            { name: '周寶盛 (K65241)', value: '5'},
-            { name: '吳泓吉 (K58963)', value: '6'},
-            { name: '鄭家豪 (K24758)', value: '7'},
-            { name: '王永慶 (K25896)', value: '8'},
-        ],
         keyword: '',  // 關鍵字
         joinTableItems: [],  // 加會人表格資料
         joinHeaders: [  // 加會人表格顯示的欄位
@@ -356,7 +260,10 @@ export default {
             { text: '讀取時間', value: 'time', align: 'center', filterable: false, divider: true, class: 'subtitle-1 white--text font-weight-bold light-blue darken-1' },
         ],
     }),
-    components: { TopBasicTable },
+    components: {
+        TopBasicTable,
+        BottomTable,
+    },
     watch: {
         // 路由參數變化時，重新向後端取資料
         $route(to, from) {
@@ -369,126 +276,33 @@ export default {
             'chLoadingShow',  // 切換 loading 圖顯示
             'closeWindow',  // 關閉視窗
         ]),
-        // 向後端取得資料
-        fetchData() {
-            this.chLoadingShow()
-            this.routeId = this.$route.params.id
-
-            // 新增測試用資料
-            setTimeout(() => {
-                let obj = {
-                    id: '111',
-                    depart: '綜合企劃科',
-                    name: '王小明',
-                    title: '3月份團康活動',
-                    desc: '說明文字說明文字說明文字說明文字說明文字說明文字說明文字說明文字說明文字說明文字說明文字說明文字說明文字說明文字說明文字',  // 說明
-                    recipients: ['2', '4', '5'],  // 收件人
-                    cc: ['6'],  // 副本
-                    joiners: ['7'],  // 加會人
-                    files: [
-                        { fileName: 'ASRC200701.jpg', link: '/demofile/demo.jpg' },
-                        { fileName: '123.docx', link: '/demofile/123.docx' },
-                        { fileName: '456.xlsx', link: '/demofile/456.xlsx' },
-                    ],
-                    opinionList: [
-                        {
-                            name: '鄭家豪',
-                            text: '意見文字意見文字意見文字意見文字意見文字意見文字意見文字意見文字意見文字意見文字意見文字意見文字意見文字意見文字意見文字意見文字',
-                        },
-                    ],
-                }
-
-                this.setShowData(obj)
-
-                // -------- 讀取追蹤 -----------
-                // 加會人
-                let Jdata = [
-                    {
-                        depart: '鐵路維護科',
-                        name: '王小明',
-                        isRead: true,
-                        time: '2020-01-05 14:22:00',
-                        hasMsg: true,
-                    },
-                ]
-
-                this.joinTableItems = Jdata.map((item, idx) => {
-                    return {
-                        num: idx + 1,
-                        depart: item.depart,
-                        name: item.name,
-                        isRead: (item.isRead)? '已讀' : '未讀',
-                        time: item.time,
-                        hasMsg: item.hasMsg,
-                    }
-                })
-
-                // 收件人
-                let Rdata = [
-                    {
-                        depart: '鐵路維護科',
-                        name: '陳弘宇',
-                        isRead: true,
-                        time: '2020-01-05 14:22:00'
-                    },
-                    {
-                        depart: '鐵路維護科',
-                        name: '王世得',
-                        isRead: true,
-                        time: '2020-01-05 15:17:00'
-                    },
-                    {
-                        depart: '鐵路維護科',
-                        name: '林捷元',
-                        isRead: false,
-                        time: ''
-                    },
-                    {
-                        depart: '鐵路維護科',
-                        name: '趙遠龍',
-                        isRead: true,
-                        time: '2020-01-05 13:42:00'
-                    },
-                    {
-                        depart: '鐵路維護科',
-                        name: '楊耀信',
-                        isRead: false,
-                        time: ''
-                    },
-                ]
-
-                this.tableItems = Rdata.map((item, idx) => {
-                    return {
-                        num: idx + 1,
-                        depart: item.depart,
-                        name: item.name,
-                        isRead: (item.isRead)? '已讀' : '未讀',
-                        time: item.time,
-                    }
-                })
-
-                this.chLoadingShow()
-            }, 1000)
-        },
         // 初始化資料
         setShowData(obj) {
-            this.topItems.depart.text = obj.depart  // 權責部門
-            this.topItems.name.text = obj.name  // 通報人
-            this.topItems.status.text = (this.closeStatus == 2)? '審核中' : ((this.closeStatus == 3)? '加會中' : '已發布')  // 發布狀態
-            this.title = obj.title // 通報主題
-            this.desc = obj.desc.replace(/\n/g, '<br>')  // 發布內容
+            this.id = obj.id  // 編號
+            this.status = obj.status  // 事故事件狀態(值)
+            this.topItems = obj.topItems  // 上面的欄位資料
+            this.bottomItems = obj.bottomItems  // 下面的欄位資料
             this.files = [ ...obj.files ]  // 檔案附件
             this.opinionList = [ ...obj.opinionList ]  // 加會意見列表
 
-            // 收件人
-            this.recipients = obj.recipients.map(item => (this.transferMemberName(item))).join('、')
-            // 副本
-            this.cc = obj.cc.map(item => (this.transferMemberName(item))).join('、')
-            // 加會人
-            this.joiners = obj.joiners.map(item => (this.transferMemberName(item))).join('、')
+            // 讀取追蹤-加會人
+            this.joinTableItems = obj.Jdata.map((item, idx) => ({
+                num: idx + 1,
+                depart: item.depart,
+                name: item.name,
+                isRead: (item.isRead)? '已讀' : '未讀',
+                time: item.time,
+                hasMsg: item.hasMsg,
+            }))
 
-            // 設定狀態(測試資料)
-            this.status = this.closeStatus
+            // 讀取追蹤-收件人
+            this.tableItems = obj.Rdata.map((item, idx) => ({
+                num: idx + 1,
+                depart: item.depart,
+                name: item.name,
+                isRead: (item.isRead)? '已讀' : '未讀',
+                time: item.time,
+            }))
         },
         // 退回
         withdraw() {
@@ -536,10 +350,6 @@ export default {
                 }, 1000)
             }
         },
-        // 員工編號轉換姓名
-        transferMemberName(id) {
-            return this.members.find(item => item.value == id).name
-        },
         // 列印
         print() {
             this.chLoadingShow()
@@ -550,7 +360,7 @@ export default {
         },
     },
     created() {
-        this.fetchData()
+        this.setShowData(this.itemData)
     }
 }
 </script>
