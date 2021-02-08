c079b48ed108b1b8dd6b98f1277b5b552de53f28
I29372 <tim.huang@icsc.com.tw>
2021-02-08 14:15:11 +0800

    update dateSelect

--- src/components/forManage/dateSelect.vue
+++ src/components/forManage/dateSelect.vue
@@ -1,15 +1,16 @@
 <!--
   輸入: 
     label: String 標題
-    iconYN: Boolean 是否要圖標，預設否 
+    showIcon: Boolean 是否要圖標，預設否 
   v-model: 綁定變數
   使用範例:
-    <dateSelect label="檢查日期(起)" v-model="input.dateStart" key="dateStart" :iconYN="變數名稱"/>
+    <dateSelect label="檢查日期(起)" v-model="input.dateStart" key="dateStart" :showIcon="true | false"/>
 -->
 <template>
   <div>
     <h3 class="mb-1">
-      <v-icon class="mr-1 mb-1" v-if="ifIcon">mdi-calendar-text</v-icon>{{label}}
+      <v-icon class="mr-1 mb-1" v-if="showIcon">mdi-calendar-text</v-icon
+      >{{ label }}
     </h3>
     <v-menu
       v-model="showYN"
@@ -38,46 +39,36 @@
 <script>
 export default {
   name: "dateSelect",
-  props:{
-    iconYN: Boolean,
+  props: {
+    showIcon: Boolean,
     label: String,
     value: String,
   },
   data: () => ({
     showYN: false,
     dataSet: "",
-    ifIcon: false,
   }),
-  components: {
-  
-  },
+  components: {},
   mounted() {
-    this.dataSet = this.value
-    this.ifIcon=(this.iconYN)?this.iconYN:this.ifIcon
+    this.dataSet = this.value;
   },
   computed: {
     // compute_name:function() {
-    
     // }
   },
-  created: function () {
-  
-  },
-  methods: {
-
-  },
+  created: function () {},
+  methods: {},
   filters: {
     // filter_name:function(value) {
-    
     // }
   },
   watch: {
-    dataSet: function(value) {
-      this.$emit('input',value)
+    dataSet: function (value) {
+      this.$emit("input", value);
     },
-    value: function(value) {
-      this.dataSet = value
-    }
-  }
-}
+    value: function (value) {
+      this.dataSet = value;
+    },
+  },
+};
 </script>
