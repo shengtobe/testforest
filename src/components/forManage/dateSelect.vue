f7aa2366b44abf7b3315da5ad8c12da5a1cdd263
IT0141 <IT0141@icsc.com.tw>
2021-02-08 12:08:16 +0800

    新增日期選擇共用元件

---
+++ src/components/forManage/dateSelect.vue
@@ -0,0 +1,79 @@
+<!--
+  輸入: 
+    label: 標題
+  v-model: 綁定變數
+  使用範例:
+    <dateSelect label="檢查日期(起)" v-model="input.dateStart" key="dateStart"/>
+-->
+<template>
+  <div>
+    <h3 class="mb-1">
+      <v-icon class="mr-1 mb-1">mdi-calendar-text</v-icon>{{label}}
+    </h3>
+    <v-menu
+      v-model="showYN"
+      :close-on-content-click="false"
+      transition="scale-transition"
+      max-width="290px"
+      min-width="290px"
+    >
+      <template v-slot:activator="{ on }">
+        <v-text-field
+          v-model.trim="dataSet"
+          solo
+          v-on="on"
+          readonly
+        ></v-text-field>
+      </template>
+      <v-date-picker
+        color="purple"
+        v-model="dataSet"
+        @input="showYN = false"
+        locale="zh-tw"
+      ></v-date-picker>
+    </v-menu>
+  </div>
+</template>
+<script>
+export default {
+  name: "dateSelect",
+  props:{
+    label: String,
+    value: String,
+  },
+  data: () => ({
+    showYN: false,
+    dataSet: "",
+  }),
+  components: {
+  
+  },
+  mounted() {
+    this.dataSet = this.value
+  },
+  computed: {
+    // compute_name:function() {
+    
+    // }
+  },
+  created: function () {
+  
+  },
+  methods: {
+
+  },
+  filters: {
+    // filter_name:function(value) {
+    
+    // }
+  },
+  watch: {
+    dataSet: function(value) {
+      this.$emit('input',value)
+    },
+    value: function(value) {
+      this.dataSet = value
+    }
+  }
+}
+</script>
