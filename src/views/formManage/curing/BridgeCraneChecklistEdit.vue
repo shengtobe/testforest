a89b33597229a02215d78b1ee8afa5d090d76ee8
IT0141 <IT0141@icsc.com.tw>
2021-02-20 10:39:27 +0800

    TB075 done

---
+++ src/views/formManage/curing/BridgeCraneChecklistEdit.vue
@@ -0,0 +1,495 @@
+<template>   
+  <v-card>
+    <v-card-title class="blue white--text px-4 py-1">
+      {{ editType }}{{ title }}
+      <v-spacer></v-spacer>
+      <v-btn dark fab small text @click="close" class="mr-n2">
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+    </v-card-title>
+    <commonQuestion v-model="inputData" :settings="setting">
+      <template v-slot:moreDetails="item">
+        <v-col cols="12" sm="4">
+          <h3 class="mb-1">編號</h3>
+          <v-text-field solo  v-model="item.editItem.SerlNo"/>
+        </v-col>
+      </template>
+    </commonQuestion>
+    <v-card-actions class="px-5 pb-5">
+      <v-btn
+        v-if="editType != actions.add"
+        elevation="4"
+        color="red"
+        class="mr-2 white--text"
+        @click="deleteRecord"
+        >刪除</v-btn
+      >
+      <v-spacer></v-spacer>
+      <v-btn class="mr-2" elevation="4" @click="close">取消</v-btn>
+      <v-btn
+        color="success"
+        elevation="4"
+        :loading="commonSettings.isLoading"
+        @click="save"
+        >送出</v-btn
+      >
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+import { mapState, mapActions } from "vuex";
+import {
+  getNowFullTime,
+  getTodayDateString,
+  encodeObject,
+  decodeObject,
+} from "@/assets/js/commonFun";
+import { generateSettings } from "@/assets/js/commonQuestion2Generater";
+import {
+  fetchFormOrderOne,
+  createFormOrder0,
+  updateFormOrder,
+} from "@/apis/formManage/serve";
+import dateSelect from "@/components/forManage/dateSelect";
+import deptSelect from "@/components/forManage/deptSelect";
+import commonQuestion from "@/components/forManage/commonQuestion2";
+import { Actions } from "@/assets/js/actions";
+import { Constrant } from "@/assets/js/constrant";
+
+export default {
+  props: {
+    item: Object,
+    editType: String,
+    DB_Table: String,
+    title: String,
+  },
+  data() {
+    return {
+      actions: Actions,
+      commonSettings: {
+        iconShow: false,
+        isLoading: false,
+        deptReadonly: true,
+      },
+      inputData: {
+        RPFlowNo: "",
+        DepartCode: "",
+        DepartName: "",
+        ID: "",
+        Name: "",
+        editableData: {
+          CheckDay: "",
+          SerlNo: "",
+          CheckOption1: "0",
+          CheckOption2: "0",
+          CheckOption3: "0",
+          CheckOption4: "0",
+          CheckOption5: "0",
+          CheckOption6: "0",
+          CheckOption7: "0",
+          CheckOption8: "0",
+          CheckOption9: "0",
+          CheckOption10: "0",
+          CheckOption11: "0",
+          CheckOption12: "0",
+          CheckOption13: "0",
+          CheckOption14: "0",
+        },
+      },
+      /*
+        subtitles: 固定,
+        textareas: 固定,
+        questions: => 1.小題產生器 2.大題產生器
+          2.產生1.的設定檔，然後2.再把1.的東西push進去array再回傳
+          小題產生器: 
+            {
+              pnaelLabel: <string>,
+              questionLabels: [
+                {
+                  width: <number>,
+                  text: <string>,
+                  generate: <boolean>,  //要被自動產生的寫ture，不要的寫false
+                },
+              ],
+              answerStart: <number>,
+              notGenerate: [            //預設不自動產生的都是純文字欄位，依照順序填入
+                [
+                  <string>,
+                ]
+              ],
+              generate: [               //按順序填入
+                {
+                  model: <變數名稱前綴 string>, //預設所有可變動資料都放在editableData裡面
+                  type: <text/number/radio/select/area string>,
+                  <number:{
+                    min: <numbebr>,
+                    max: <number>,
+                  },>
+                  <radio: [
+                    {
+                      color: <string>,
+                      label: <string>,
+                      value: <string>,
+                    }
+                  ],> 
+                  <select: [
+                    {
+                      text: <string>,
+                      value: <string>,
+                    }
+                  ],>
+                },
+              ]
+            }
+          大題產生器
+            {
+              panelQuestion: [      //題目
+                {
+                  panelLabel: <string>,
+                  questions: [
+                    [
+                      <string>,
+                    ],
+                  ],
+                }
+              ],
+              questionLabels: [       //標題
+                {
+                  width: <number>,
+                  text: <string>,
+                  generate: <boolean>,  //要被自動產生的寫ture，不要的寫false
+                },
+              ],
+              panelAnswer: [      //輸入欄位
+                {
+                  model: <變數名稱前綴 string>, //預設所有可變動資料都放在editableData裡面
+                  type: <text/number/radio/select/area string>,
+                  <number:{
+                    min: <numbebr>,
+                    max: <number>,
+                  },>
+                  <radio: [
+                    {
+                      color: <string>,
+                      label: <string>,
+                      value: <string>,
+                    }
+                  ],> 
+                  <select: [
+                    {
+                      text: <string>,
+                      value: <string>,
+                    }
+                  ],>
+                },
+              ]
+            }
+      */
+      testSettings: {
+        subtitles: [
+          "1.依職業安全衛生法第23條及職業安全衛生管理辦法第52條規定辦理。",
+          "2.檢查結果請依狀態選擇正常、異常或無此項目。",
+          "3.缺點由使用單位自行改善，不克者委請設備商維護。",
+          "4.本表於月底前完成檢查，經主管核章後，留存於管理單位之系統以保存備查。",
+        ],
+        questions: {
+          panelQuestion: [      //題目
+            {
+              panelLabel: "過捲預防裝置",
+              questions: [
+                [
+                  "1. 吊物升降極限開關",
+                  "動作",
+                  "動作是否確實螺絲有否鬆弛",
+                ],
+              ],
+            },
+            {
+              panelLabel: "剎車控制器",
+              questions: [
+                [
+                  "1.電磁升降剎車",
+                  "動作",
+                  "反覆操作上升查看其動作狀況停止位置",
+                ],
+                [
+                  "2.電磁走行剎車",
+                  "動作",
+                  "反覆操作上升查看其動作狀況停止位置",
+                ],
+                [
+                  "3.渦電流或其他控制下降剎車",
+                  "動作",
+                  "查看降落速度是否適當",
+                ],
+                [
+                  "4.直接控器",
+                  "動作",
+                  "反覆操作查看其動作狀況",
+                ],
+                [
+                  "5.間接控制器",
+                  "動作",
+                  "反覆操作查看其動作狀況及電譯情形",
+                ],
+                [
+                  "6.軌道",
+                  "損傷斷裂",
+                  "檢視",
+                ],
+                [
+                  "7.吊運車橫行導軌",
+                  "損傷斷裂",
+                  "檢視",
+                ],
+                [
+                  "8.軌道上障礙物",
+                  "目視",
+                  "是否移開",
+                ],
+              ],
+            },
+            {
+              panelLabel: "鋼纜",
+              questions: [
+                [
+                  "1. 鋼纜",
+                  "有無異狀",
+                  "檢視",
+                ],
+              ],
+            },
+            {
+              panelLabel: "警報裝置",
+              questions: [
+                [
+                  "1.行走警鈴",
+                  "響亮",
+                  "行走時響亮",
+                ],
+                [
+                  "2.警報器",
+                  "響亮",
+                  "按動時鳴叫",
+                ],
+              ],
+            },
+            {
+              panelLabel: "吊鉤",
+              questions: [
+                [
+                  "1.吊鉤",
+                  "損傷",
+                  "檢視有無損傷斷裂",
+                ],
+                [
+                  "2.吊鉤安全栓",
+                  "損傷",
+                  "檢視有無損傷斷裂",
+                ],
+              ],
+            },
+          ],
+          questionLabels: [       //標題
+            {
+              width: 3,
+              text: "檢查部位",
+              generate: false,
+            },
+            {
+              width: 3,
+              text: "檢查內容",
+              generate: false,
+            },
+            {
+              width: 3,
+              text: "檢查方法",
+              generate: false,
+            },
+            {
+              width: 3,
+              text: "結果",
+              generate: true,
+            },
+          ],
+          panelAnswer: [      //輸入欄位
+            {
+              type: "radio",
+              model: "CheckOption",
+              radio: [
+                {
+                  color: "success",
+                  label: "正常",
+                  value: "1",
+                },
+                {
+                  color: "red",
+                  label: "異常",
+                  value: "2",
+                },
+                {
+                  color: "black",
+                  label: "無此項目",
+                  value: "3",
+                },
+              ]
+            },
+          ]
+        }
+      },
+      setting: {}
+    }
+  },
+  components: {
+    dateSelect,
+    deptSelect,
+    commonQuestion,
+  },
+  mounted() {
+    this.editType == this.actions.edit
+      ? this.viewPage(this.item)
+      : this.newPage();
+    this.setting = generateSettings(this.testSettings)
+  },
+  computed: {
+    ...mapState("user", {
+      userData: (state) => state.userData, // 使用者基本資料
+    }),
+  },
+  methods: {
+    ...mapActions("system", [
+      "chMsgbar", // messageBar
+      "chLoadingShow", // 切換 loading 圖顯示
+    ]),
+    newPage() {
+      this.inputData.editableData.CheckDay = getTodayDateString();
+      this.inputData.Name = this.userData.UserName;
+      this.inputData.ID = this.userData.UserId;
+      this.inputData.DepartCode = this.userData.DeptList[0].DeptId;
+      this.inputData.DepartName = this.userData.DeptList[0].DeptDesc;
+    },
+    viewPage(item) {
+      const that = this;
+      this.chLoadingShow();
+      fetchFormOrderOne({
+        ClientReqTime: getNowFullTime(), // client 端請求時間
+        OperatorID: this.userData.UserId, // 操作人id
+        KeyName: this.DB_Table, // DB table
+        KeyItem: [{ Column: "RPFlowNo", Value: item.RPFlowNo }],
+        QyName: [
+          "CheckDay",
+          "DepartCode",
+          "DepartName",
+          "ID",
+          "Name",
+          "SerlNo",
+          "CheckOption1",
+          "CheckOption2",
+          "CheckOption3",
+          "CheckOption4",
+          "CheckOption5",
+          "CheckOption6",
+          "CheckOption7",
+          "CheckOption8",
+          "CheckOption9",
+          "CheckOption10",
+          "CheckOption11",
+          "CheckOption12",
+          "CheckOption13",
+          "CheckOption14",
+        ],
+      })
+        .then((res) => {
+          let dat = JSON.parse(res.data.DT);
+          dat[0].CheckDay = dat[0].CheckDay.substr(0, 10);
+          this.inputData.RPFlowNo = this.item.RPFlowNo;
+          this.inputData.DepartCode = dat[0].DepartCode;
+          this.inputData.Name = dat[0].Name;
+          dat[0] = decodeObject(dat[0]);
+          const inputArr = Object.keys(this.inputData.editableData);
+          inputArr.forEach((e) => {
+            that.inputData.editableData[e] = dat[0][e];
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          this.chMsgbar({ success: false, msg: Constrant.query.failed });
+        })
+        .finally(() => {
+          this.chLoadingShow();
+        });
+    },
+    close() {
+      this.$emit("close");
+      this.$emit("search");
+    },
+    save() {
+      const that = this;
+      let rtnObj = [];
+      const keyArr = Object.keys(that.inputData.editableData);
+      keyArr.forEach((e) => {
+        rtnObj.push({ Column: e, Value: that.inputData.editableData[e] });
+      });
+      encodeObject(rtnObj);
+      if (this.editType == this.actions.add) {
+        createFormOrder0({
+          ClientReqTime: getNowFullTime(), // client 端請求時間
+          OperatorID: this.userData.UserId, // 操作人id
+          FunCode: "C",
+          KeyName: this.DB_Table, // DB table
+          KeyItem: rtnObj,
+        })
+          .then((res) => {
+            if (res.data.ErrorCode == 0) {
+              this.chMsgbar({ success: true, msg: Constrant.insert.success });
+            } else {
+              sessionStorage.errData = JSON.stringify({
+                errCode: res.data.Msg,
+                msg: res.data.Msg,
+              });
+              that.$router.push({ path: "/error" });
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+            this.chMsgbar({ success: false, msg: Constrant.insert.failed });
+          })
+          .finally(() => {
+            that.close();
+          });
+      } else {
+        //就是edit
+        updateFormOrder({
+          ClientReqTime: getNowFullTime(), // client 端請求時間
+          OperatorID: this.userData.UserId, // 操作人id
+          RPFlowNo: that.inputData.RPFlowNo,
+          FunCode: "U",
+          KeyName: this.DB_Table, // DB table
+          KeyItem: rtnObj,
+        })
+          .then((res) => {
+            if (res.data.ErrorCode == 0) {
+              this.chMsgbar({ success: true, msg: Constrant.update.success });
+            } else {
+              sessionStorage.errData = JSON.stringify({
+                errCode: res.data.Msg,
+                msg: res.data.Msg,
+              });
+              that.$router.push({ path: "/error" });
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+            this.chMsgbar({ success: false, msg: Constrant.update.failed });
+          })
+          .finally(() => {
+            that.close();
+          });
+      }
+    },
+    deleteRecord() {
+      this.$emit("deleteRecord", this.inputData.RPFlowNo);
+    },
+  },
+}
+</script>
