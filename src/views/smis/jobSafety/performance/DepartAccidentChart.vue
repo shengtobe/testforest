1d1728813e73349db4cc0b6c9411529a1810c3b5
IT0141 <IT0141@icsc.com.tw>
2021-06-10 15:16:03 +0800

    職安績效管理

--- src/views/smis/jobSafety/performance/DepartAccidentChart.vue
+++ src/views/smis/jobSafety/performance/DepartAccidentChart.vue
@@ -1,18 +1,28 @@
 <template>
 <v-container style="max-width: 1200px">
     <h2 class="mb-4 label-title">科室職災事故統計</h2>
-
+    <v-row>
+        <v-col>
+            <dateSelect label="統計日期(起)" v-model="input.dateStart" key="dateStart" :showIcon="true"/>
+        </v-col>
+        <v-col>
+            <dateSelect label="統計日期(訖)" v-model="input.dateEnd" key="dateStart" :showIcon="true"/>
+        </v-col>
+        <v-col>
+            <v-btn
+                dark
+                class="my-8 btn-search"
+                @click="getData"
+            >查詢</v-btn>
+        </v-col>
+    </v-row>
     <v-row class="px-2 mb-8">
         <v-col cols="12" md="6">
-            <canvas ref="canvas1" style="background-color: white;"
-                v-show="show"
-            ></canvas>
+            <chart v-if="chartLeft.componentKey>1" :chartdata="chartLeft.chartdata" :options="chartLeft.options" :key="chartLeft.componentKey"/>
         </v-col>
 
         <v-col cols="12" md="6">
-            <canvas ref="canvas2" style="background-color: white;"
-                v-show="show"
-            ></canvas>
+            <chart v-if="chartRight.componentKey>1" :chartdata="chartRight.chartdata" :options="chartRight.options" :key="chartRight.componentKey"/>
         </v-col>
 
         <v-col cols="12">
@@ -26,124 +36,169 @@
 
 <script>
 import Chart from 'chart.js'
-import { mapActions } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 import { departAccidentChartData } from '@/assets/js/smisTestData'
-
+import dateSelect from '@/components/forManage/dateSelect2'
+import chart from '@/components/chartBar'
+import { analyDangerByDepart } from '@/apis/smis/jobSafety'
+import { getNowFullTime } from '@/assets/js/commonFun'
 export default {
     data: () => ({
         show: false,  // canvas 是否隱藏(在還沒有資料時會變小)
+        input:{
+            dateStart:'',
+            dateEnd:''
+        },
+        chartLeft:{
+            componentKey: 1,
+            options: {
+                title: {  // 標題
+                    display: true,
+                    text: '件數',
+                    fontSize: 16,
+                },
+                scales: {  // 側邊訊息
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,  // y 軸最小數字從 0 開始
+                            // max: Math.max(...data.counter)+2,  // y 軸最大數為資料的最大數+2
+                        }
+                    }]
+                },
+                layout: {
+                    padding: {
+                        left: 15,
+                        right: 20,
+                        top: 20,
+                        bottom: 20,
+                    }
+                },
+                legend: {  // 上方的題示小色塊(又稱圖例，在 title 下方)
+                    display: false,  // 隱藏
+                },
+                tooltips: {  // 圖表上的資訊題示
+                    intersect: false,  // 滑鼠移到 x 軸上就顯示
+                },  
+            },
+            chartdata:{
+                labels: [],
+                datasets: [
+                    {
+                        backgroundColor: [],  // 節點的顏色
+                        data: [],
+                        barPercentage: 0.5,
+                        fill: 20,
+                    },
+                ]
+            }
+        },
+        chartRight:{
+            componentKey: 1,
+            options: {
+                title: {  // 標題
+                    display: true,
+                    text: '損失日數(公傷假)',
+                    fontSize: 16,
+                },
+                scales: {  // 側邊訊息
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,  // y 軸最小數字從 0 開始
+                            // max: Math.max(...data.counter)+2,  // y 軸最大數為資料的最大數+2
+                        }
+                    }]
+                },
+                layout: {
+                    padding: {
+                        left: 15,
+                        right: 20,
+                        top: 20,
+                        bottom: 20,
+                    }
+                },
+                legend: {  // 上方的題示小色塊(又稱圖例，在 title 下方)
+                    display: false,  // 隱藏
+                },
+                tooltips: {  // 圖表上的資訊題示
+                    intersect: false,  // 滑鼠移到 x 軸上就顯示
+                },  
+            },
+            chartdata:{
+                labels: [],
+                datasets: [
+                    {
+                        backgroundColor: [],  // 節點的顏色
+                        data: [],
+                        barPercentage: 0.5,
+                        fill: 20,
+                    },
+                ]
+            }
+        },
     }),
+    components:{
+        dateSelect,
+        chart
+    },
+    computed: {
+        ...mapState ('user', {
+            userData: state => state.userData,  // 使用者基本資料
+        }),
+    },
     methods: {
         ...mapActions('system', [
+            'chMsgbar',  // 改變 messageBar
             'chLoadingShow',  // 切換 loading 圖顯示
+            'chViewDialog',  // 檢視內容 dialog
         ]),
         // 初始化資料
         initData() {
+            const today = new Date
+            this.input.dateEnd = today.getFullYear() + '-' + (today.getMonth()+1<10?'0':'') + (today.getMonth()+1) + '-' + today.getDate()
+            this.getData()
+        },
+        getData() {
             this.chLoadingShow()
-
-            // 測試用資料
-            setTimeout(() => {
-                // 向後端取得資料後
-                this.createChart(departAccidentChartData)  // 繪製圖表
-                this.show = true
+            analyDangerByDepart({
+                ClientReqTime: getNowFullTime(),  // client 端請求時間
+                OperatorID: this.userData.UserId,  // 操作人id
+                CreateDTime_Start:this.input.dateStart,
+                CreateDTime_End:this.input.dateEnd
+            }).then(res=>{
+                if (res.data.ErrorCode == 0) {
+                    console.log(res.data)
+                    res.data.SumCount.forEach(e=>{
+                        this.chartLeft.chartdata.labels.push(e.DepName)
+                        this.chartLeft.chartdata.datasets[0].backgroundColor.push(this.getRandomColor())
+                        this.chartLeft.chartdata.datasets[0].data.push(e.Count)
+                    })
+                    this.chartLeft.options.scales.yAxes[0].ticks.max = Math.max(...this.chartLeft.chartdata.datasets[0].data)+2
+                    this.chartLeft.componentKey++
+                    res.data.DayCount.forEach(e=>{
+                        this.chartRight.chartdata.labels.push(e.DepName)
+                        this.chartRight.chartdata.datasets[0].backgroundColor.push(this.getRandomColor())
+                        this.chartRight.chartdata.datasets[0].data.push(e.Count)
+                    })
+                    this.chartRight.options.scales.yAxes[0].ticks.max = Math.max(...this.chartRight.chartdata.datasets[0].data)+2
+                    this.chartRight.componentKey++
+                }else{
+                    sessionStorage.errData = JSON.stringify({ errCode: res.data.Msg, msg: res.data.Msg })
+                    this.$router.push({ path: '/error' })
+                }
                 this.chLoadingShow()
-            }, 1000)
-        },
-        // 繪製圖表
-        createChart(data) {
-            let ctx1 = this.$refs.canvas1
-            let myChart1 = new Chart(ctx1, {
-                type: 'bar',
-                data: {
-                    labels: data.labels,
-                    datasets: [
-                        {
-                            backgroundColor: '#00e00a',  // 顏色
-                            data: data.counter,
-                            barPercentage: 0.5,
-                            fill: 20,
-                        },
-                    ]
-
-                },
-                options: {
-                    title: {  // 標題
-                        display: true,
-                        text: '件數',
-                        fontSize: 16,
-                    },
-                    scales: {  // 側邊訊息
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true,  // y 軸最小數字從 0 開始
-                                max: Math.max(...data.counter)+2,  // y 軸最大數為資料的最大數+2
-                            }
-                        }]
-                    },
-                    layout: {
-                        padding: {
-                            left: 15,
-                            right: 20,
-                            top: 20,
-                            bottom: 20,
-                        }
-                    },
-                    legend: {  // 上方的題示小色塊(又稱圖例，在 title 下方)
-                        display: false,  // 隱藏
-                    },
-                    tooltips: {  // 圖表上的資訊題示
-                        intersect: false,  // 滑鼠移到 x 軸上就顯示
-                    },
-                }
-            })
-
-            // 損失日數
-            let ctx2 = this.$refs.canvas2
-            let myChart2 = new Chart(ctx2, {
-                type: 'bar',
-                data: {
-                    labels: data.labels,
-                    datasets: [
-                        {
-                            backgroundColor: '#8a98ff',  // 顏色
-                            data: data.loss,
-                            barPercentage: 0.5,
-                            fill: 20,
-                        },
-                    ]
-
-                },
-                options: {
-                    title: {  // 標題
-                        display: true,
-                        text: '損失日數(公傷假)',
-                        fontSize: 16,
-                    },
-                    scales: {  // 側邊訊息
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true,  // y 軸最小數字從 0 開始
-                                max: Math.max(...data.loss)+10,  // y 軸最大數為資料的最大數+2
-                            }
-                        }]
-                    },
-                    layout: {
-                        padding: {
-                            left: 15,
-                            right: 20,
-                            top: 20,
-                            bottom: 20,
-                        }
-                    },
-                    legend: {  // 上方的題示小色塊(又稱圖例，在 title 下方)
-                        display: false,  // 隱藏
-                    },
-                    tooltips: {  // 圖表上的資訊題示
-                        intersect: false,  // 滑鼠移到 x 軸上就顯示
-                    },
-                }
+            }).catch( err => {
+                console.error(err)
+                this.chMsgbar({ success: false, msg: '伺服器發生問題，資料讀取失敗' })
             })
         },
+        getRandomColor() {
+            var letters = '0123456789ABCDEF';
+            var color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }
     },
     mounted() {
         this.initData()
--- src/views/smis/jobSafety/performance/HarmTypeChart.vue
+++ src/views/smis/jobSafety/performance/HarmTypeChart.vue
@@ -1,14 +1,28 @@
 <template>
 <v-container style="max-width: 1200px">
     <h2 class="mb-4 label-title">職災傷害類型統計</h2>
-    
+    <v-row>
+        <v-col>
+            <dateSelect label="統計日期(起)" v-model="input.dateStart" key="dateStart" :showIcon="true"/>
+        </v-col>
+        <v-col>
+            <dateSelect label="統計日期(訖)" v-model="input.dateEnd" key="dateStart" :showIcon="true"/>
+        </v-col>
+        <v-col>
+            <v-btn
+                dark
+                class="my-8 btn-search"
+                @click="getData"
+            >查詢</v-btn>
+        </v-col>
+    </v-row>
     <p class="label-warning">* 此圖各數字的單位為「%」</p>
 
     <!-- 圖表 -->
-    <canvas ref="canvas" style="background-color: white;"
+    <!-- <canvas ref="canvas" style="background-color: white;"
         v-show="show"
-    ></canvas>
-
+    ></canvas> -->
+    <chart v-if="chart.componentKey>1" :chartdata="chart.chartdata" :options="chart.options" :key="chart.componentKey"/>
     <v-btn
         dark
         class="my-8 btn-close"
@@ -19,66 +33,107 @@
 
 <script>
 import Chart from 'chart.js'
-import { mapActions } from 'vuex'
+import { mapState,mapActions } from 'vuex'
 import { harmTypeChartData } from '@/assets/js/smisTestData'
-
+import { analyDanger } from '@/apis/smis/jobSafety'
+import dateSelect from '@/components/forManage/dateSelect2'
+import chart from '@/components/chartPie'
+import { getNowFullTime } from '@/assets/js/commonFun'
 export default {
     data: () => ({
         show: false,  // canvas 是否隱藏(在還沒有資料時會變小)
+        chart:{
+            componentKey: 1,
+            options: {
+                scales: {  // 側邊訊息
+                    yAxes: [{
+                        display: false,  // 不顯示 Y 軸
+                    }]
+                },
+                layout: {
+                    padding: {
+                        left: 15,
+                        right: 20,
+                        top: 20,
+                        bottom: 20,
+                    }
+                }, 
+                legend: {  // 上方的題示小色塊(又稱圖例，在 title 下方)
+                    
+                },
+                tooltips: {  // 圖表上的資訊題示
+                    intersect: false,  // 滑鼠移到 x 軸上就顯示
+                },
+            },
+            chartdata:{
+                labels: [],
+                    datasets: [
+                        {
+                            backgroundColor: [],  // 節點的顏色
+                            data: [],
+                        },
+                    ]
+            }
+        },
+        input:{
+            dateStart:'',
+            dateEnd:''
+        } 
     }),
+    components:{
+        dateSelect,
+        chart
+    },
+    computed: {
+        ...mapState ('user', {
+            userData: state => state.userData,  // 使用者基本資料
+        }),
+    },
     methods: {
         ...mapActions('system', [
+            'chMsgbar',  // 改變 messageBar
             'chLoadingShow',  // 切換 loading 圖顯示
+            'chViewDialog',  // 檢視內容 dialog
         ]),
         // 初始化資料
         initData() {
-            this.chLoadingShow()
-
-            // 測試用資料
-            setTimeout(() => {
-                // 向後端取得資料後
-                this.createChart(harmTypeChartData)  // 繪製圖表
-                this.show = true
-                this.chLoadingShow()
-            }, 1000)
+            const today = new Date
+            this.input.dateEnd = today.getFullYear() + '-' + (today.getMonth()+1<10?'0':'') + (today.getMonth()+1) + '-' + today.getDate()
+            this.getData()
         },
-        // 繪製圖表
-        createChart(data) {
-            let ctx = this.$refs.canvas
-            let myChart = new Chart(ctx, {
-                type: 'pie',
-                data: {
-                    labels: data.labels,
-                    datasets: [
-                        {
-                            backgroundColor: ['#EC407A', '#039BE5', '#FFB300', '#00f03f'],  // 節點的顏色
-                            data: data.data,
-                        },
-                    ]
-                },
-                options: {
-                    scales: {  // 側邊訊息
-                        yAxes: [{
-                            display: false,  // 不顯示 Y 軸
-                        }]
-                    },
-                    layout: {
-                        padding: {
-                            left: 15,
-                            right: 20,
-                            top: 20,
-                            bottom: 20,
-                        }
-                    }, 
-                    legend: {  // 上方的題示小色塊(又稱圖例，在 title 下方)
-                        
-                    },
-                    tooltips: {  // 圖表上的資訊題示
-                        intersect: false,  // 滑鼠移到 x 軸上就顯示
-                    },
+        getData() {
+            this.chLoadingShow()
+            analyDanger({
+                ClientReqTime: getNowFullTime(),  // client 端請求時間
+                OperatorID: this.userData.UserId,  // 操作人id
+                CreateDTime_Start:this.input.dateStart,
+                CreateDTime_End:this.input.dateEnd
+            }).then(res=>{
+                if (res.data.ErrorCode == 0) {
+                    console.log(res.data.DataList)
+                    res.data.DataList.forEach(e=>{
+                        this.chart.chartdata.labels.push(e.Name)
+                        this.chart.chartdata.datasets[0].backgroundColor.push(this.getRandomColor())
+                        this.chart.chartdata.datasets[0].data.push(parseFloat(e.Value.replace(/'%'/g,'')))
+                    })
+                    this.chart.componentKey++
+                }else{
+                    sessionStorage.errData = JSON.stringify({ errCode: res.data.Msg, msg: res.data.Msg })
+                    this.$router.push({ path: '/error' })
                 }
+                this.chLoadingShow()
+            }).catch( err => {
+                this.chMsgbar({ success: false, msg: '伺服器發生問題，資料讀取失敗' })
             })
         },
+        getRandomColor() {
+            var letters = '0123456789ABCDEF';
+            var color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }
     },
     mounted() {
         this.initData()
